85dba765a56e133ac64eafbbb06fea34caa0260b
ajay7701 <ajaymourya770@gmail.com>
2021-09-11 10:35:46 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import Login from "./components/Login.jsx"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          {/* <Route
+            exact
+            path="/"
+            render={() => {
+              return <Redirect to="/" />;
+            }}
+          /> */}
+        </Switch>
+      </Router>
+    </>
   );
 }
 
